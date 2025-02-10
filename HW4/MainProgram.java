d67b4acefd8a42067bc21ae74766ac1266c4ffec
Siyan <w8115405@gmail.com>
2025-02-10 12:10:55 -0500

    4update

--- HW4/MainProgram.java
+++ HW4/MainProgram.java
@@ -2,8 +2,22 @@ import java.util.concurrent.locks.ReentrantLock;
 
 class Counter {
     private int count = 0;
+    
+    // This method is not synchronized, which allows race conditions
+    public void increment() {
+        count++;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+}
+
+class SynchronizedCounter {
+    private int count = 0;
     private final ReentrantLock lock = new ReentrantLock();
     
+    // Thread-safe method using ReentrantLock
     public void increment() {
         lock.lock();
         try {
@@ -42,21 +56,23 @@ class Singleton {
 public class MainProgram {
     public static void main(String[] args) throws InterruptedException {
         runCounterTest();
+        runSynchronizedCounterTest();
         runSingletonTest();
         runDeadlockTest();
     }
     
+    // Test case for counter thread interference issue (without synchronization)
     private static void runCounterTest() throws InterruptedException {
         Counter counter = new Counter();
         
         Thread t1 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 10000; i++) {
                 counter.increment();
             }
         });
         
         Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 10000; i++) {
                 counter.increment();
             }
         });
@@ -67,9 +83,38 @@ public class MainProgram {
         t1.join();
         t2.join();
         
-        System.out.println("Final count: " + counter.getCount());
+        // Expected output should be 2000, but due to thread interference,
+        // the result will often be lower than 20000.
+        System.out.println("Final count (without synchronization): " + counter.getCount());
     }
     
+    // Test case for counter with synchronization
+    private static void runSynchronizedCounterTest() throws InterruptedException {
+        SynchronizedCounter counter = new SynchronizedCounter();
+        
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+        
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+        
+        t1.start();
+        t2.start();
+        
+        t1.join();
+        t2.join();
+        
+        // Expected output should always be 20000 due to synchronization.
+        System.out.println("Final count (with synchronization): " + counter.getCount());
+    }
+    
+    // Test case for verifying singleton instance in a multi-threaded environment
     private static void runSingletonTest() {
         Thread t3 = new Thread(() -> {
             Singleton singleton1 = Singleton.getInstance();
@@ -83,8 +128,11 @@ public class MainProgram {
         
         t3.start();
         t4.start();
+        
+        // Both threads should print the same hashCode, proving that only one instance is created
     }
     
+    // Test case demonstrating a potential deadlock situation and solution
     private static void runDeadlockTest() {
         Object key1 = new Object();
         Object key2 = new Object();
@@ -99,7 +147,7 @@ public class MainProgram {
         });
         
         Thread t9 = new Thread(() -> {
-            synchronized (key1) {
+            synchronized (key1) { // Changed order to avoid deadlock
                 System.out.println("t9 has key 1.");
                 synchronized (key2) {
                     System.out.println("t9 has key 2");
@@ -109,5 +157,7 @@ public class MainProgram {
         
         t8.start();
         t9.start();
+        
+        // Expected output: Both threads should acquire locks in a consistent order to avoid deadlock
     }
 }
